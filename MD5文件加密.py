e96c736d006e7aa4cf6e5d8cc7b770ca6129a86e
wangxiaoying <wangxiaoying@icourt.cc>
2019-02-11 17:37:51 +0800

    MD5加密文件

---
+++ MD5文件加密.py
@@ -0,0 +1,25 @@
+# coding=gbk
+import hashlib
+import os
+import datetime
+
+def GetFileMd5(filename):
+    if not os.path.isfile(filename):
+        return
+    myhash = hashlib.md5()
+    f = open(filename,'rb')
+    while True:
+        b = f.read(8096)
+        if not b :
+            break
+        myhash.update(b)
+    f.close()
+    return myhash.hexdigest()
+
+filepath = input('请输入文件路径：')
+
+# 输出文件的md5值以及记录运行时间
+starttime = datetime.datetime.now()
+print(GetFileMd5(filepath))
+endtime = datetime.datetime.now()
+print ('运行时间：%ds'%((endtime-starttime).seconds))
